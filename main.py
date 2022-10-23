70159c60789c4ea8ab5ef8e227183fc78630590f
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2022-10-23 20:49:11 +0200

    Applying LinearRegression algorithm to a basic dataset

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import numpy as np
 import pandas as pd
+from sklearn.linear_model import LinearRegression
 
 anime_list_data_path = "./datasets/AnimeList.csv"
 user_mal_data_path = "./datasets/UserMAL.csv"
@@ -26,8 +28,22 @@ def get_mal_data():
     return dataset
 
 
-def get_recommendation_dataset():
-    mal_data = get_mal_data()
+def get_mal_basic_data():
+    anime_list_data = pd.read_csv(anime_list_data_path)
+    dataset = dict()
+    for anime_data in anime_list_data.iterrows():
+        anime = anime_data[1]
+        dataset[anime["anime_id"]] = [anime["episodes"],
+                                      anime["score"],
+                                      anime["scored_by"],
+                                      anime["popularity"],
+                                      anime["members"],
+                                      anime["favorites"],
+                                      ]
+    return dataset
+
+
+def get_recommendation_dataset(_data):
     user_mal_data = pd.read_csv(user_mal_data_path)
 
     _x_train = []
@@ -39,15 +55,23 @@ def get_recommendation_dataset():
         anime_user_score = user_anime["my_score"]
 
         if anime_user_score != 0:
-            if anime_id in mal_data.keys():
-                _x_train.append(mal_data.pop(anime_id))
+            if anime_id in _data.keys():
+                _x_train.append(_data.pop(anime_id))
                 _y_train.append(anime_user_score)
 
-    return _x_train, _y_train, list(mal_data)
+    return _x_train, _y_train, list(_data.values())
 
 
 if __name__ == '__main__':
-    x_train, y_train, x_test = get_recommendation_dataset()
-    print("x_train:", len(x_train))
-    print("x_test:", len(x_test))
-    print(y_train)
+    data = get_mal_basic_data()
+    x_train, y_train, x_test = get_recommendation_dataset(data.copy())
+
+    print("nb of train data:", len(x_train))
+    print("dim of the train data:", len(x_train[0]))
+    print("example of sample", x_train[0])
+
+    lr = LinearRegression()
+    lr.fit(x_train, y_train)
+    print(lr.predict([data[32901]]))
+
+
