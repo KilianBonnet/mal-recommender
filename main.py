0c681db9d43951af5e7ff2a10c3d5e8adc82f908
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2022-10-23 12:49:27 +0200

    Adding Estoult MAL profile as user_mal_data

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import pandas as pd
 
 anime_list_data_path = "./datasets/AnimeList.csv"
+user_mal_data_path = "./datasets/Estoult.csv"
 
 
 def get_mal_dataset():
     return pd.read_csv(anime_list_data_path)
 
 
+def get_user_mal():
+    return pd.read_csv(user_mal_data_path)
+
+
 if __name__ == '__main__':
     mal_dataset = get_mal_dataset()
+    user_mal = get_user_mal()
+    print(user_mal)
